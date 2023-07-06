ee0d914388b7d8436667900f51a9a9dac310a204
1800tolu <tolusa@hotmail.com>
2023-07-06 21:56:30 +0100

    edits

--- serialize-instruction-data/solana-movie-frontend/components/MovieList.tsx
+++ serialize-instruction-data/solana-movie-frontend/components/MovieList.tsx
@@ -1,54 +1,25 @@
 import { Card } from './Card'
-import { FC, useEffect, useMemo, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { Movie } from '../models/Movie'
-import * as web3 from '@solana/web3.js'
-import { MovieCoordinator } from '../coordinators/MovieCoordinator'
-import { Button, Center, HStack, Input, Spacer } from '@chakra-ui/react'
+
+const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 
 export const MovieList: FC = () => {
-    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [movies, setMovies] = useState<Movie[]>([])
-    const [page, setPage] = useState(1)
-    const [search, setSearch] = useState('')
 
     useEffect(() => {
-        MovieCoordinator.fetchPage(
-            connection, 
-            page, 
-            5,
-            search,
-            search !== ''
-        ).then(setMovies)
-    }, [page, search])
+        setMovies(Movie.mocks)
+    }, [])
     
     return (
         <div>
-            <Center>
-                <Input
-                    id='search'
-                    color='gray.400'
-                    onChange={event => setSearch(event.currentTarget.value)}
-                    placeholder='Search'
-                    w='97%'
-                    mt={2}
-                    mb={2}
-                />
-            </Center>
             {
-                movies.map((movie, i) => <Card key={i} movie={movie} /> )
+                movies.map((movie, i) => {
+                    return (
+                        <Card key={i} movie={movie} />
+                    )
+                })
             }
-            <Center>
-                <HStack w='full' mt={2} mb={8} ml={4} mr={4}>
-                    {
-                        page > 1 && <Button onClick={() => setPage(page - 1)}>Previous</Button>
-                    }
-                    <Spacer />
-                    {
-                        MovieCoordinator.accounts.length > page * 5 &&
-                        <Button onClick={() => setPage(page + 1)}>Next</Button>
-                    }
-                </HStack>
-            </Center>
         </div>
     )
 }
